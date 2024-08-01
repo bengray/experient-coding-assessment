cc9c919a5cc70cbd06d5e1fc86f1354768342cd1
Ben Gray <bendoylegray@gmail.com>
2024-08-01 00:06:26 -0400

    Add another test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,37 @@
 import {
     render,
-    within
+    waitFor,
+    within,
+    screen,
+    prettyDOM,
+    fireEvent
 } from '@testing-library/react'
 import App from './App'
 import userEvent from "@testing-library/user-event"
+// import mockUsersList from './mockData/mockUsersList'
 
 describe("AutoComplete Tests", () => {
   it("Test that input field can accept user input", async () => {
-    const { getByTestId } = render(<App />, {})
+    render(<App />, {})
 
-    const autocomplete = getByTestId('autocomplete-search')
+    const autocomplete = screen.getByTestId('autocomplete-search')
     const input = within(autocomplete).getByRole('combobox')
 
     userEvent.type(input, 'Ben')
     expect(input).toHaveValue('Ben')
   })
+
+  it('Renders options', async () => {
+    render(<App />, {})
+
+    await waitFor(() => expect(screen.getAllByText('Start typing to filter users')).toBeDefined());
+
+    const combobox = screen.getByRole('combobox');
+
+    fireEvent.change(combobox, { target: { value: 'Bauch, Clementine' } });
+
+    const listbox = await waitFor(() => screen.getByRole('listbox'), { timeout: 2000 });
+
+    expect(within(listbox).queryAllByRole('option').length).toBeGreaterThan(0);
+  });
 })
